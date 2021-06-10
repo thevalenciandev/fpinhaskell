c19efb1717d568b9bbef83c0b42d8aca3f033996
TVD <thevalenciandev@gmail.com>
2021-06-10 19:15:44 +0100

    Tree: sorted fn

--- week3/Tree.hs
+++ week3/Tree.hs
@@ -7,3 +7,9 @@ treeDepth (Node _ r l) = 1 + max (treeDepth r) (treeDepth l)
 treeSum :: Tree -> Int
 treeSum Leaf         = 0
 treeSum (Node x r l) = x + treeSum r + treeSum l
+
+isSortedTree :: Tree -> Int -> Int -> Bool
+isSortedTree Leaf _ _ = True
+isSortedTree (Node x l r) min max
+    | x <= min || x > max = False
+    | otherwise           = isSortedTree l min x && isSortedTree r x max
