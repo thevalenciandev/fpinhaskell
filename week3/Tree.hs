c016ae18b3afcdda691b967dca989ea181fcf799
TVD <thevalenciandev@gmail.com>
2021-06-10 18:34:26 +0100

    Tree: sum fn

--- week3/Tree.hs
+++ week3/Tree.hs
@@ -3,3 +3,7 @@ data Tree = Leaf | Node Int Tree Tree
 treeDepth :: Tree -> Int
 treeDepth Leaf         = 0
 treeDepth (Node _ r l) = 1 + max (treeDepth r) (treeDepth l)
+
+treeSum :: Tree -> Int
+treeSum Leaf         = 0
+treeSum (Node x r l) = x + treeSum r + treeSum l
