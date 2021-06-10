11f184cff1c3b9e8b29a02c380592eebcb3789f2
TVD <thevalenciandev@gmail.com>
2021-06-10 20:49:46 +0100

    Tree: add new max fn

--- week3/Tree.hs
+++ week3/Tree.hs
@@ -1,4 +1,4 @@
-data Tree = Leaf | Node Int Tree Tree
+data Tree = Leaf | Node Int Tree Tree deriving Show
 
 treeDepth :: Tree -> Int
 treeDepth Leaf         = 0
@@ -13,3 +13,8 @@ isSortedTree Leaf _ _ = True
 isSortedTree (Node x l r) min max
     | x <= min || x > max = False
     | otherwise           = isSortedTree l min x && isSortedTree r x max
+
+addNewMax :: Tree -> Tree
+addNewMax Leaf = Node 0 Leaf Leaf
+addNewMax (Node x l Leaf) = Node x l (Node (x+1) Leaf Leaf)
+addNewMax (Node x l r)    = Node x l (addNewMax r)
